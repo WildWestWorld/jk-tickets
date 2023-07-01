0e3801ed52aa631d83285b8cb8e8c9a9071c2df9
JOTARO <380686356@qq.com>
2023-07-01 17:30:20 +0800

    利用HuTool 生成JWT

--- member-service/src/main/java/com/jktickets/service/impl/MemberServiceImpl.java
+++ member-service/src/main/java/com/jktickets/service/impl/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.jwt.JWTUtil;
 import com.jktickets.domain.Member;
 import com.jktickets.domain.MemberExample;
 import com.jktickets.exception.BusinessException;
@@ -21,6 +22,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MemberServiceImpl implements MemberService {
@@ -29,7 +31,6 @@ public class MemberServiceImpl implements MemberService {
 
     @Resource
     MemberMapper memberMapper;
-
 
 
     @Override
@@ -75,7 +76,7 @@ public class MemberServiceImpl implements MemberService {
 
 
 //        创建ORM条件查询
-       Member memberDB = SelectMemberByMobile(mobile);
+        Member memberDB = SelectMemberByMobile(mobile);
 //        如果手机号不存在 则插入一条数据
         if (ObjectUtil.isNull(memberDB)) {
             LOG.info("手机号不存在,则插入数据");
@@ -86,7 +87,7 @@ public class MemberServiceImpl implements MemberService {
             member.setMobile(mobile);
 
             memberMapper.insert(member);
-        }else {
+        } else {
             LOG.info("手机号存在,不插入数据");
         }
 
@@ -94,7 +95,7 @@ public class MemberServiceImpl implements MemberService {
 //        随机四位数的字符串
         String code = RandomUtil.randomString(4);
 //        String code = "8888";
-        LOG.info("生成短信验证码:{}",code);
+        LOG.info("生成短信验证码:{}", code);
 
 //        保存短信记录表:手机号，短信验证码，有效期，是否已使用，业务类型，发送时间，使用时间
         LOG.info("保存短信记录表");
@@ -117,21 +118,30 @@ public class MemberServiceImpl implements MemberService {
         }
 
         //校验短信验证码
-        if(!"8888".equals(code)){
+        if (!"8888".equals(code)) {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
 
         }
-//        利用HuTool进行复制
+
+ //        利用HuTool进行复制
 //        copyProperties(要复制的数据,要转化成的类)
         MemberLoginRes memberLoginRes = BeanUtil.copyProperties(memberDB, MemberLoginRes.class);
 
 
+//        利用huTool生成JWT
+//        1.拿到Payload   用户信息转成Map
+        Map<String, Object> map = BeanUtil.beanToMap(memberLoginRes);
+//        2.设置秘钥
+        String key = "JK12306";
+//        3.huTool生成Token createToken(Map<String, Object> payload, byte[] key)
+        String token = JWTUtil.createToken(map, key.getBytes());
+        memberLoginRes.setToken(token);
         return memberLoginRes;
 
 
     }
 
-//    子函数
+    //    子函数
     private Member SelectMemberByMobile(String mobile) {
         //        创建ORM条件查询
         MemberExample memberExample = new MemberExample();
@@ -142,7 +152,7 @@ public class MemberServiceImpl implements MemberService {
 //        没有查询到数据
         if (CollUtil.isEmpty(memberList)) {
             return null;
-        }else {
+        } else {
             return memberList.get(0);
         }
 
