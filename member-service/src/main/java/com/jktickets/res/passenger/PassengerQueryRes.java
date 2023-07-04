f47da9a198145c1b56890d718973bdbb24883071
JOTARO <380686356@qq.com>
2023-07-04 22:11:07 +0800

    解决前端 id 为long类型的精度问题

--- member-service/src/main/java/com/jktickets/res/passenger/PassengerQueryRes.java
+++ member-service/src/main/java/com/jktickets/res/passenger/PassengerQueryRes.java
@@ -1,13 +1,18 @@
 package com.jktickets.res.passenger;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.util.Date;
 @Data
 public class PassengerQueryRes {
-    private Long id;
 
+//   @JsonSerialize(using= ToStringSerializer.class) 发给前端时转成String类型 解决前端精度问题
+    @JsonSerialize(using= ToStringSerializer.class)
+    private Long id;
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long memberId;
 
     private String name;
