21ad53231cc8184863f401e19a286ae69bb73abf
JOTARO <380686356@qq.com>
2023-07-22 12:18:17 +0800

    扣减余票数量，并判断余票是否足够

--- business-service/src/main/java/com/jktickets/service/impl/ConfirmOrderServiceImpl.java
+++ business-service/src/main/java/com/jktickets/service/impl/ConfirmOrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.jktickets.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
@@ -12,11 +13,15 @@ import com.jktickets.domain.ConfirmOrder;
 import com.jktickets.domain.ConfirmOrderExample;
 import com.jktickets.domain.DailyTrainTicket;
 import com.jktickets.enums.ConfirmOrderStatusEnum;
+import com.jktickets.enums.SeatTypeEnum;
+import com.jktickets.exception.BusinessException;
+import com.jktickets.exception.BusinessExceptionEnum;
 import com.jktickets.mapper.ConfirmOrderMapper;
 
 import com.jktickets.req.confirmOrder.ConfirmOrderDoReq;
 import com.jktickets.req.confirmOrder.ConfirmOrderQueryReq;
 import com.jktickets.req.confirmOrder.ConfirmOrderSaveReq;
+import com.jktickets.req.confirmOrder.ConfirmOrderTicketReq;
 import com.jktickets.res.PageRes;
 import com.jktickets.res.confirmOrder.ConfirmOrderQueryRes;
 
@@ -29,6 +34,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -48,13 +54,13 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
     public void saveConfirmOrder(ConfirmOrderSaveReq req) {
 
 
-        DateTime nowTime  = DateTime.now();
+        DateTime nowTime = DateTime.now();
 
 
         ConfirmOrder confirmOrder = BeanUtil.copyProperties(req, ConfirmOrder.class);
 
 
-        if(ObjectUtil.isNull(confirmOrder.getId())){
+        if (ObjectUtil.isNull(confirmOrder.getId())) {
             //        从 线程中获取数据
 //          confirmOrder.setMemberId(LoginMemberContext.getId());
             confirmOrder.setId(SnowUtil.getSnowflakeNextId());
@@ -62,11 +68,10 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
             confirmOrder.setUpdateTime(nowTime);
 
             confirmOrderMapper.insert(confirmOrder);
-        }else{
+        } else {
             confirmOrder.setUpdateTime(nowTime);
             confirmOrderMapper.updateByPrimaryKeySelective(confirmOrder);
         }
-
 
 
     }
@@ -110,7 +115,7 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
         // 省略业务数据校验，如：车次是否存在，余票是否存在，车次是否在有效期内，tickets条数>0，同乘客同车次是否已买过
 
         // 保存确认订单表，状态初始
-        DateTime nowTime  = DateTime.now();
+        DateTime nowTime = DateTime.now();
 
         ConfirmOrder confirmOrder = new ConfirmOrder();
         confirmOrder.setId(SnowUtil.getSnowflakeNextId());
@@ -138,10 +143,12 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
         // 查出余票记录，需要得到真实的库存
 //        根据日期，火车编号，出发地，目的地
         DailyTrainTicket dailyTrainTicket = dailyTrainTicketService.selectByUnique(date, trainCode, start, end);
-        LOG.info("查出余票记录:{}",dailyTrainTicket);
+        LOG.info("查出余票记录:{}", dailyTrainTicket);
 
+
+        List<ConfirmOrderTicketReq> ticketReqList = req.getTickets();
         // 扣减余票数量，并判断余票是否足够
-
+        reduceTickets(dailyTrainTicket, ticketReqList);
         // 选座
 
         // 一个车箱一个车箱的获取座位数据
@@ -154,5 +161,51 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
         // 余票详情表修改余票；
         // 为会员增加购票记录
         // 更新确认订单为成功
+    }
+
+    private static void reduceTickets(DailyTrainTicket dailyTrainTicket, List<ConfirmOrderTicketReq> ticketReqList) {
+        //  循环获取的票类型，
+        for (ConfirmOrderTicketReq ticketReq : ticketReqList) {
+//seatTypeCode =1(一等座)，2（二等座）
+            String seatTypeCode = ticketReq.getSeatTypeCode();
+//          SeatTypeEnum::getCode =   YDZ("1",), EDZ("2"）
+//
+//            EnumUtil.getBy( 查询的对象,查询的内容 )
+            SeatTypeEnum seatTypeEnum = EnumUtil.getBy(SeatTypeEnum::getCode, seatTypeCode);
+//查询的结果seatTypeCode =YDZ =》   seatTypeEnum ={YDZ("1")}
+
+//            这样我们就获取到对应的 车票类型 这样我们就能取出库存 数量并减一
+            switch (seatTypeEnum) {
+                case YDZ -> {
+                    int countRemain = dailyTrainTicket.getYdz() - 1;
+                    if (countRemain < 0) {
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    dailyTrainTicket.setYdz(countRemain);
+                }
+                case EDZ -> {
+                    int countRemain = dailyTrainTicket.getEdz() - 1;
+                    if (countRemain < 0) {
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    dailyTrainTicket.setEdz(countRemain);
+                }
+                case RW -> {
+                    int countRemain = dailyTrainTicket.getRw() - 1;
+                    if (countRemain < 0) {
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    dailyTrainTicket.setRw(countRemain);
+
+                }
+                case YW -> {
+                    int countRemain = dailyTrainTicket.getYw() - 1;
+                    if (countRemain < 0) {
+                        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_TICKET_COUNT_ERROR);
+                    }
+                    dailyTrainTicket.setYw(countRemain);
+                }
+            }
+        }
     }
 }
