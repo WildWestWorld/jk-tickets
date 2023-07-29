001cff29919295cac8b71a466986b734f3e9d492
JOTARO <380686356@qq.com>
2023-07-29 22:21:35 +0800

    继承 Nacos配置中心 实现动态刷新功能

--- member-service/src/main/java/com/jktickets/controller/TestController.java
+++ member-service/src/main/java/com/jktickets/controller/TestController.java
@@ -2,11 +2,14 @@ package com.jktickets.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RefreshScope 加上了Nacos的配置文件改变 对应的value值也会实时更新
+@RefreshScope
 public class TestController {
     @Value("${test.nacos}")
     String testNacos;
